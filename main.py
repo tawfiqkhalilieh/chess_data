b2e1fd249f2b75197baaeb9e9c72c91f9cb261d5
xtawfeeq7k <tawfeeqkhalilia51@gmail.com>
2023-05-16 11:24:56 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from render_board import render_board
+import chess.engine
+import chess.pgn
+import chess.polyglot
+
+thinking_time = 0.1
+
+stockfish = chess.engine.SimpleEngine.popen_uci("stockfish.exe")
+# komodo = chess.engine.SimpleEngine.popen_uci("komodo.exe")
+batata = chess.engine.SimpleEngine.popen_uci("batata_engine/lc0.exe")
+
+
+def data_writer(board, game_number):
+    long_string = " ".join([str(move) for move in board.move_stack])
+    print(long_string)
+    with open(f"data/{thinking_time}/game{game_number}.txt", "w") as f:
+        f.write(long_string)
+
+
+count = 347
+
+while True:
+    board = chess.Board()
+    try:
+        while not board.is_game_over():
+            board.push(stockfish.play(board, chess.engine.Limit(time=thinking_time )).move)
+            render_board(board, thinking_time )
+            if not board.is_game_over():
+                board.push(batata.play(board, chess.engine.Limit(time=thinking_time )).move)
+                render_board(board, thinking_time )
+    except:
+        stockfish = chess.engine.SimpleEngine.popen_uci("stockfish.exe")
+        batata = chess.engine.SimpleEngine.popen_uci("batata_engine/lc0.exe")
+
+    data_writer(board=board, game_number=count)
+    count += 1
+
+    board = chess.Board()
+    try:
+        while not board.is_game_over():
+            board.push(batata.play(board, chess.engine.Limit(time=thinking_time )).move)
+            render_board(board, thinking_time )
+            if not board.is_game_over():
+                board.push(stockfish.play(board, chess.engine.Limit(time=thinking_time )).move)
+                render_board(board, thinking_time )
+    except:
+        stockfish = chess.engine.SimpleEngine.popen_uci("stockfish.exe")
+        batata = chess.engine.SimpleEngine.popen_uci("batata_engine/lc0.exe")
+
+    data_writer(board=board, game_number=count)
+    count += 1
+
+batata.quit()
+stockfish.quit()
---
+++ render_board.py
@@ -0,0 +1,12 @@
+import chess.svg
+
+
+def render_board(chess_board, game_number):
+    with open(f"ui/game-{game_number}.svg", "w") as file:
+        if chess.svg.board:
+            file.write(
+                chess.svg.board(
+                    chess_board,
+                    size=1000,
+                )
+            )
